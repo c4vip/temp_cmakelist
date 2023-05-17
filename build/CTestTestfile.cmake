922bf8802d4e3b17808dd064017f567a3bf33c07
c4vip <c4vip@outlook.com>
2023-05-17 14:19:39 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/temp_cmakelist
+# Build directory: /root/temp_cmakelist/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <boost/asio.hpp>
+#include <spdlog/spdlog.h>
+#include <curl/curl.h>
+#include <json/json.h>
+#include <openssl/ssl.h>
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
