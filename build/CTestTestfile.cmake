4f36b5862c69a63a62ca3876d8d07499d3412cc5
c4vip <c4vip@outlook.com>
2023-05-17 14:23:20 +0800

    update

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/temp_cmakelist
-# Build directory: /root/temp_cmakelist/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
